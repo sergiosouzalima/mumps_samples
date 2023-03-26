6d822492cdf70c6f5de29f5dfed38462ddd419be
Sergio Lima <sergiosouzalima@gmail.com>
2023-03-26 16:56:21 -0300

    Updating repo

--- getFieldInput.m
+++ getFieldInput.m
@@ -1,8 +1,8 @@
 	; 
 	; It creates a field to read and set data to a variable;
 	;
-	; File mame: getFieldInput.m
-	; Author: Sergio Lima (Mar, 25 2023)
+	; File mame: getFieldInput. ;
+	; Author: Sergio Lima (Mar, 26 2023)
 	;
 	; ---
 	; If needed:
@@ -18,57 +18,74 @@ INI ;
 	D PROCESS
 	W "***",!
 	Q
-	;
-OBT(C,L,T,CM,LM,CAMPO,ATMSGER,MSGER,ATMSGINF,MSGINF,EXPREG,ATVALINI)
-	;
-	; ATMSGER =1 Ativa mostrar mensagem de preenchimento errado
-	;         =0 Nao mostra mensagem de preenchimento errado (default)
-	; MSGER   =  Mensagem caso o preenchimento esteja errado
-	;         =  "Valor preenchido incorretamente. Tente de novo." (default)
-	;
-	; ATMSGINF=1 Ativa mostrar mensagem de informacao para preenchimento
-	;         =0 Nao mostra mensagem de informacao para preenchimento (default)
-	; MSGINF  =  Mensagem de informacao para preenchimento
-	;         =  "Preencha o campo corretamente." (default)
-	; 
-	; EXPREG  = Expressao regular para validar esse campo
-	;         = Sem expressao regular. (default)
-	;
-	; ATVALINI=1 Ativa assumir valor inicial do campo caso usuario saia com "."
-	;         =0 Nao assumir valor inicial do campo (default)
-	;
-	N NOVCAMP,TIMEOUT,CMDSAI,TAMCAMP
-	S NOVCAMP="",TIMEOUT=10,CMDSAI=""
-	; Ajuste de valores default para os parametros
-	S ATMSGER=$G(ATMSGER,0),MSGER=$G(MSGER) 
-	I MSGER="" S MSGER="Valor preenchido incorretamente. Tente de novo."
-	S ATMSGINF=$G(ATMSGINF,0),MSGINF=$G(MSGINF) 
-	I MSGINF="" S MSGINF="Preencha o campo corretamente."
-	S EXPREG=$G(EXPREG),ATEXRG=1 I EXPREG="" S ATEXRG=0
-	S ATVALINI=$G(ATVALINI,0)
 	;	
-	I ATMSGINF D POS(CM,LM) W MSGINF
-	D POS(C,L)
+OBT(C,L,T,CM,LM,VTPARAM)
 	;
-	S INVALCMP=1
-	S CMDSAI=0
-	F  Q:'INVALCMP!(CMDSAI)  D
-	. R NOVCAMP#T+1:TIMEOUT
-	. S CMDSAI=(NOVCAMP=".")
-	. Q:CMDSAI
-	. S TAMCAMP=$L(NOVCAMP)
-	. S INVALCMP=0
-	. I ATEXRG D
-	. . S INVALCMP='(NOVCAMP?@(EXPREG))
-	. I INVALCMP D 
-	. . I ATMSGER D POS(CM,LM) W MSGER_" <ENTER>" R X
-	. . D POS(CM,LM) W $J(" ",80)
-	. . I ATMSGINF D POS(CM,LM) W MSGINF
-	. . S NOVCAMP=""
-	. . D POS(C,L) W $J(" ",T) D POS(C,L)
-	I CMDSAI&(ATVALINI) S NOVCAMP=CAMPO
+	; VTPARAM: Vetor para recebimento de parametros. ;
+	; Parametros:
+	N IEXRG    ; Indicador para ativar expressao regular
+	N EXPREG   ; Expressao regular
+	N IMSGERRO ; Indicador para ativar mensagem de erro
+	N MSGERRO  ; Mensagem de erro de preenchimento
+	N IMSGINF  ; Indicador para ativar mensagem de orientacao para preenchimento do campo
+	N MSGINF   ; Mensagem de orientacao para preenchimento do campo
+	N IMANDTR  ; Indicador para ativar campo mandatorio
+	N MSGMAND  ; Mensagem para avisar que o campo e' mandatorio
 	;
-FOBT Q NOVCAMP
+	N CAMPO
+	;
+	S (IEXRG,IMSGERRO,IMSGINF,IMANDTR)=0
+	S MSGINF="Informe o campo corretamente."
+	S MSGERRO="Valor invalido. Tente novamente."
+	S MSGMAND="Campo de preenchimento obrigatorio."
+	S EXPREG=""
+	;
+	; Ajusta valores iniciais
+	I $G(VTPARAM("EXPREG"))'="" S IEXRG=1,EXPREG=VTPARAM("EXPREG") ; Ajuste para expressao regular
+	I $G(VTPARAM("IMANDTR"))'="" S IMANDTR=VTPARAM("IMANDTR")      ; Ajuste para campo mandatorio
+	I $G(VTPARAM("IMSGINF"))'="" S IMSGINF=VTPARAM("IMSGINF")      ; Ajuste para msg de pre-preenchimento
+	I IMSGINF I $G(VTPARAM("MSGINF"))'="" S MSGINF=VTPARAM("MSGINF")
+	I $G(VTPARAM("IMSGERRO"))'="" S IMSGERRO=VTPARAM("IMSGERRO")   ; Ajuste para mensagem de erro de preenchimento
+	I IMSGERRO I $G(VTPARAM("MSGERRO"))'="" S MSGERRO=VTPARAM("MSGERRO")
+	;
+	S FIMLER=0
+	F  Q:FIMLER  D
+	. I IMSGINF D POS(CM,LM) W $J(" ",80) D POS(CM,LM) W MSGINF
+	. D POS(C,L) W $J(" ",T+1) D POS(C,L)
+	. S CAMPO=""
+	. D LECAMPO(.CAMPO,T) ; Leitura do campo
+	. I CAMPO="." S FIMLER=1 Q
+	. I ('IMANDTR)&('IEXRG) S FIMLER=1 Q ; Campo nao e' mandatorio e nao tem expressao regular, nao tem o que validar.... ;
+	. I ('IMANDTR)&($L(CAMPO)=0) S FIMLER=1 Q ; Campo nao e' mandatorio e esta vazio, nao tem o que validar.... 	
+	. D VALID(CAMPO,.FIMLER,IEXRG,EXPREG,IMANDTR,IMSGERRO,MSGERRO) ; Validacao
+	. D POS(CM,LM) W $J(" ",80)
+	;
+FOBT Q CAMPO
+	;
+LECAMPO(CAMPO,TAM)
+	N TIMEOUT
+	S TIMEOUT=10
+	R CAMPO#TAM+1:TIMEOUT	
+	;
+FLECAMPO Q 
+	;
+VALID(CAMPO,FIMLER,IEXRG,EXPREG,IMANDTR,IMSGERRO,MSGERRO) ; Leitura e Validacao
+	N CMPVALID,CMPVAZIO
+	S CMPVALID=0
+	S CMPVAZIO=0
+	S FIMLER=1
+	;
+	S CMPVAZIO=($L(CAMPO)=0)
+	I (IMANDTR)&(CMPVAZIO) D  Q
+	. D POS(CM,LM) W $J(" ",80) D POS(CM,LM) W MSGMAND_"<ENTER>" R X D POS(C,L)
+	. S FIMLER=0
+	;
+	I IEXRG D  Q
+	. S CMPVALID=(CAMPO?@(EXPREG))
+	. I ('CMPVALID)&(IMSGERRO) D POS(CM,LM) W $J(" ",80) D POS(CM,LM) W MSGERRO_"<ENTER>" R X D POS(C,L)
+	. I 'CMPVALID S FIMLER=0
+	;
+FOBT1 Q
 	;
 POS(C,L)
 	U $i:(X=C:Y=L)
@@ -80,12 +97,13 @@ TEST ;
 	D POS(10,10)
 	W "Entre com um valor:"
 	S TAM=30,NOVO="",NOME="SERGIO"
-	S ATMSGER=1
-	S MSGER="Valor invalido. Informe nome de 5 a 30 caracteres."
-	S ATMSGINF=1
-	S MSGINF="Informe nome de 5 a 30 caracteres."
-	S ATVALINI=1
-	S NOVO=$$OBT(30,10,TAM,10,20,NOME,ATMSGER,MSGER,ATMSGINF,MSGINF,"5E.25E",ATVALINI)
+	S VTPAR("EXPREG")="5E.25E"
+	S VTPAR("IMANDTR")=1
+	S VTPAR("IMSGINF")=1
+	S VTPAR("MSGINF")="Informe nome de 5 a 30 caracteres."
+	S VTPAR("IMSGERRO")=1
+	S VTPAR("MSGERRO")="Valor invalido. Informe nome de 5 a 30 caracteres."
+	S NOVO=$$OBT(30,10,TAM,10,20) ;,.VTPAR)
 	;
 	W !
 	W NOVO
